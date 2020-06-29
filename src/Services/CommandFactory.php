2377ce3663b54a0294ddcf80252ecd5ab928214f
Jon Cram <webignition@gmail.com>
2020-06-29 17:46:57 +0100

    Import command (#17)

---
+++ src/Services/CommandFactory.php
@@ -0,0 +1,17 @@
+<?php
+
+declare(strict_types=1);
+
+namespace webignition\BasilCliRunner\Services;
+
+use webignition\BasilCliRunner\Command\RunCommand;
+
+class CommandFactory
+{
+    public static function createRunCommand(): RunCommand
+    {
+        $projectRootPath = (new ProjectRootPathProvider())->get();
+
+        return new RunCommand($projectRootPath, new ConsoleOutputFormatter());
+    }
+}
