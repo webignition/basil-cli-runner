a9517b7cda895e7dbe89f712fca7f71524fbeac2
Jon Cram <webignition@gmail.com>
2020-06-30 10:40:21 +0100

    Create application

---
+++ src/Services/ApplicationFactory.php
@@ -0,0 +1,29 @@
+<?php
+
+declare(strict_types=1);
+
+namespace webignition\BasilCliRunner\Services;
+
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Console\SingleCommandApplication;
+
+class ApplicationFactory
+{
+    public static function create(): SingleCommandApplication
+    {
+        $command = CommandFactory::createRunCommand();
+
+        $application = new SingleCommandApplication();
+        $application->setName((string) $command->getName());
+        $application->setDefinition($command->getDefinition());
+
+        $application
+            ->setVersion('0.1-beta')
+            ->setCode(function (InputInterface $input, OutputInterface $output) use ($command) {
+                return $command->run($input, $output);
+            });
+
+        return $application;
+    }
+}
