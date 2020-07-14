7df624cab983c4e2ff0adc573ee3c0a1d7bb3571
Jon Cram <webignition@gmail.com>
2020-07-14 17:51:19 +0100

    Use latest tag name as app version when compiling phar

---
+++ src/Services/PharBuilder.php
@@ -0,0 +1,52 @@
+<?php
+
+declare(strict_types=1);
+
+namespace webignition\BasilCliRunner\Services;
+
+use webignition\SingleCommandApplicationPharBuilder\Builder;
+
+class PharBuilder
+{
+    public function build(string $root, string $binPath): void
+    {
+        $latestGitTag = trim(shell_exec('git describe --abbrev=0 --tags'));
+
+        $this->setBinRunnerVersion($binPath, $latestGitTag);
+        $this->executeBuild($root);
+        $this->revertBinRunnerChanges($binPath);
+    }
+
+    private function setBinRunnerVersion(string $binPath, string $version): void
+    {
+        $binContent = file_get_contents($binPath);
+
+        if (is_string($binContent)) {
+            $binContent = str_replace(
+                'const VERSION = \'dev-master\';',
+                'const VERSION = \'' . $version . '\';',
+                $binContent
+            );
+
+            file_put_contents($binPath, $binContent);
+        }
+    }
+
+    private function executeBuild(string $root): void
+    {
+        (new Builder(
+            $root,
+            'build/runner.phar',
+            'bin/runner',
+            [
+                'src',
+                'vendor',
+            ]
+        ))->build();
+    }
+
+    private function revertBinRunnerChanges(string $binPath): void
+    {
+        shell_exec('git checkout -- ' . $binPath);
+    }
+}
