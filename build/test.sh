e7314c30b14d916f93ab355dfdbf671ecf15b7f1
Jon Cram <webignition@gmail.com>
2020-08-07 16:56:00 +0100

    Update test port

--- build/test.sh
+++ build/test.sh
@@ -5,7 +5,7 @@ source ${CURRENT_DIRECTORY}/.image_data.sh
 
 CONTAINER_NAME="test-runner-container"
 CONTAINER_PORT="8000"
-HOST_PORT="9000"
+HOST_PORT="9001"
 
 docker rm -f ${CONTAINER_NAME}
 docker create -p ${HOST_PORT}:${CONTAINER_PORT} --name ${CONTAINER_NAME} ${IMAGE_NAME}
