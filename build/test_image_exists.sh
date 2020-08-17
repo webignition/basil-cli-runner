3195f1d1e63fe0725e3d015ab270fda9cf9346df
Jon Cram <webignition@gmail.com>
2020-08-17 17:21:29 +0100

    Verify existence of built images

--- build/test_image_exists.sh
+++ build/test_image_exists.sh
@@ -1,13 +1,15 @@
 #!/usr/bin/env bash
 
 CURRENT_DIRECTORY="$(dirname "$0")"
-source ${CURRENT_DIRECTORY}/.image_data.sh
+source ${CURRENT_DIRECTORY}/../build/.image_data.sh
 
-OUTPUT=$(docker images | tail -n +2 | awk '{print $1":"$2}' | grep ${IMAGE_NAME} | wc -l)
+for IMAGE_NAME in "${IMAGE_NAMES[@]}"; do
+  OUTPUT=$(docker images | tail -n +2 | awk '{print $1":"$2}' | grep ${IMAGE_NAME} | wc -l)
 
-if [ ${OUTPUT} != "1" ]; then
-  echo "x" ${IMAGE_NAME} "does not exist"
-  exit 1
-fi
+  if [ ${OUTPUT} != "1" ]; then
+    echo "x" ${IMAGE_NAME} "does not exist"
+    exit 1
+  fi
 
-echo "✓" ${IMAGE_NAME} "exists"
+  echo "✓" ${IMAGE_NAME} "exists"
+done
