4d81a9bd24cf9b7597ae976ef5bb3dd57397dcb6
Jon Cram <webignition@gmail.com>
2021-02-16 10:17:27 +0000

    Store generated browser-specific dockerfiles in repo

--- build-dockerfile.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-set -u
-: "${BROWSER}"
-
-echo "Browser: ${BROWSER}"
-cat ./docker/template.before.Dockerfile ./docker/${BROWSER}.partial.Dockerfile ./docker/template.after.Dockerfile > ./Dockerfile
