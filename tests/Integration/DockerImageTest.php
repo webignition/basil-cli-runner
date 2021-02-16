61c4506011aee3efb896b1bf1e909fc9b81697ad
Jon Cram <webignition@gmail.com>
2021-02-16 16:49:04 +0000

    Update integration tests

--- tests/Integration/DockerImageTest.php
+++ tests/Integration/DockerImageTest.php
@@ -6,187 +6,88 @@ namespace webignition\BasilCliRunner\Tests\Integration;
 
 use PHPUnit\Framework\TestCase;
 use Symfony\Component\Console\Output\BufferedOutput;
-use webignition\BasilCliRunner\Tests\Model\Docker\BrowserContainer;
-use webignition\BasilCliRunner\Tests\Model\Docker\BrowserRunner;
-use webignition\BasilCliRunner\Tests\Model\Docker\BrowserRunners;
-use webignition\BasilCliRunner\Tests\Model\Docker\CompilerContainer;
-use webignition\BasilCliRunner\Tests\Model\Docker\Container;
-use webignition\BasilCliRunner\Tests\Model\Docker\ContainerInterface;
-use webignition\BasilCliRunner\Tests\Model\Docker\Network;
-use webignition\BasilCliRunner\Tests\Model\ProcessRunResult\ProcessRunResultInterface;
+use Symfony\Component\Yaml\Yaml;
+use webignition\BasilCompilerModels\SuiteManifest;
 use webignition\TcpCliProxyClient\Client;
 use webignition\TcpCliProxyModels\Output;
 
 class DockerImageTest extends TestCase
 {
-    private const COMPILER_IMAGE = 'smartassert/basil-compiler:0.25';
-    private const NGINX_IMAGE = 'nginx:1.19';
+    private const COMPILER_SOURCE_PATH = '/app/source';
+    private const COMPILER_TARGET_PATH = '/app/target';
+    private const COMPILER_PORT = 9500;
 
-    private const RUNNER_PATH = '/app/generated';
+    private const CHROME_RUNNER_PORT = 9501;
+    private const FIREFOX_RUNNER_PORT = 9502;
 
-    private const COMPILER_CONTAINER_NAME = 'test-compiler-container';
-    private const NGINX_CONTAINER_NAME = 'test-nginx-container';
-    private const NETWORK_NAME = 'test-network';
-
-    private const EXPECTED_RUNNER_BROWSER_NAMES = [
-        'chrome',
-        'firefox',
-    ];
-
-    private const EXPECTED_BROWSER_RUNNER_COUNT = 2;
     private const EXPECTED_STEP_PASSED_COUNT = 4;
     private const EXPECTED_STATEMENT_PASSED_COUNT = 12;
 
-    private static CompilerContainer $compilerContainer;
-    private static ContainerInterface $nginxContainer;
-    private static Network $network;
-
     /**
-     * @var BrowserRunners
+     * @dataProvider runTestInBrowserRunnerDataProvider
      */
-    private static BrowserRunners $browserRunners;
-
-    public static function setUpBeforeClass(): void
+    public function testRunTestInBrowserRunner(string $source, int $runnerPort): void
     {
-        parent::setUpBeforeClass();
+        $suiteManifest = $this->compileSource($source);
+        $testManifests = $suiteManifest->getTestManifests();
+        $testManifest = $testManifests[0];
+        $testPath = $testManifest->getTarget();
 
-        self::$browserRunners = new BrowserRunners(self::getImageTag());
-        self::assertCount(self::EXPECTED_BROWSER_RUNNER_COUNT, self::$browserRunners);
-        self::assertSame(self::EXPECTED_RUNNER_BROWSER_NAMES, self::$browserRunners->getNames());
-
-        self::createCompilerContainer();
-        self::createNginxContainer();
-        self::createNetwork();
-
-        self::$network->connect(self::$nginxContainer);
-    }
-
-    public static function tearDownAfterClass(): void
-    {
-        parent::tearDownAfterClass();
-
-        self::$compilerContainer->remove();
-        self::$nginxContainer->remove();
-        self::$network->remove();
-    }
-
-    /**
-     * @dataProvider runnerDataProvider
-     */
-    public function testRunner(BrowserRunner $browserRunner)
-    {
-        $browser = $browserRunner->getBrowserName();
-        $browserRunnerContainerName = 'test-' . $browser . '-container';
-
-        $suiteManifest = self::$compilerContainer->compileForBrowserRunner($browserRunner);
-        $testManifest = $suiteManifest->getTestManifests()[0];
-        $browserRunnerContainer = self::createBrowserRunnerContainer($browserRunnerContainerName, $browserRunner);
-        self::$network->connect($browserRunnerContainer);
-
-        $browserRunnerClient = Client::createFromHostAndPort(
-            'localhost',
-            (int) $browserRunnerContainer->getLocalPort()
-        );
+        $browserRunnerClient = Client::createFromHostAndPort('localhost', $runnerPort);
 
         $browserRunnerClientOutput = new BufferedOutput();
         $browserRunnerClient = $browserRunnerClient->withOutput($browserRunnerClientOutput);
 
         $browserRunnerClient->request(sprintf(
             './bin/runner --path=%s/%s',
-            self::RUNNER_PATH,
-            basename($testManifest->getTarget())
+            self::COMPILER_TARGET_PATH,
+            basename($testPath)
         ));
 
         $outputContent = $browserRunnerClientOutput->fetch();
+
         $outputObject = Output::fromString($outputContent);
         self::assertSame(0, $outputObject->getExitCode());
         self::assertRunnerOutput($outputObject);
+    }
 
-        $removeBrowserContainerResult = $browserRunnerContainer->remove();
-        self::assertProcessResult($removeBrowserContainerResult);
+    /**
+     * @return array<mixed>
+     */
+    public function runTestInBrowserRunnerDataProvider(): array
+    {
+        return [
+            'chrome' => [
+                'source' => self::COMPILER_SOURCE_PATH . '/chrome-open-index.yml',
+                'runnerPort' => self::CHROME_RUNNER_PORT,
+            ],
+            'firefox' => [
+                'source' => self::COMPILER_SOURCE_PATH . '/firefox-open-index.yml',
+                'runnerPort' => self::FIREFOX_RUNNER_PORT,
+            ],
+        ];
+    }
 
-        $generatedFilesToDelete = glob($browserRunner->getLocalTargetPath() . '/*.php');
-        if (is_array($generatedFilesToDelete)) {
-            foreach ($generatedFilesToDelete as $generatedFile) {
-                unlink($generatedFile);
-            }
+    private function compileSource(string $source): SuiteManifest
+    {
+        $output = new BufferedOutput();
+        $compilerClient = Client::createFromHostAndPort('localhost', self::COMPILER_PORT);
+        $compilerClient = $compilerClient->withOutput($output);
+
+        $compilerClient->request(sprintf(
+            './compiler --source=%s --target=%s',
+            $source,
+            self::COMPILER_TARGET_PATH
+        ));
+
+        $output = Output::fromString($output->fetch());
+        if (0 !== $output->getExitCode()) {
+            throw new \RuntimeException($output->getContent(), $output->getExitCode());
         }
-    }
 
-    public function runnerDataProvider(): array
-    {
-        $browserRunners = new BrowserRunners(self::getImageTag());
-        self::assertCount(self::EXPECTED_BROWSER_RUNNER_COUNT, $browserRunners);
+        $manifestData = Yaml::parse($output->getContent());
 
-        $dataSets = [];
-
-        foreach ($browserRunners->get() as $browserRunner) {
-            $data = [
-                'browserRunner' => $browserRunner,
-            ];
-
-            $dataSets[$browserRunner->getImageName()] = $data;
-        }
-
-        return $dataSets;
-    }
-
-    private static function assertProcessResult(ProcessRunResultInterface $result): void
-    {
-        self::assertTrue($result->isSuccessful(), $result->getErrorOutput());
-    }
-
-    private static function getImageTag(): string
-    {
-        return $_ENV['TRAVIS_BRANCH'] ?? 'master';
-    }
-
-    private static function createCompilerContainer(): void
-    {
-        self::$compilerContainer = new CompilerContainer(
-            self::COMPILER_CONTAINER_NAME,
-            self::COMPILER_IMAGE,
-            self::$browserRunners
-        );
-
-        $setupResult = self::$compilerContainer->setup();
-        self::assertProcessResult($setupResult);
-    }
-
-    private static function createNginxContainer(): void
-    {
-        self::$nginxContainer = new Container(
-            self::NGINX_CONTAINER_NAME,
-            self::NGINX_IMAGE,
-            [
-                '-v ' . getcwd() . '/build/test/html:/usr/share/nginx/html',
-            ]
-        );
-
-        $setupResult = self::$nginxContainer->setup();
-        self::assertProcessResult($setupResult);
-    }
-
-    private static function createNetwork(): void
-    {
-        self::$network = new Network(self::NETWORK_NAME);
-        $createResult = self::$network->create();
-        self::assertProcessResult($createResult);
-    }
-
-    private static function createBrowserRunnerContainer(string $name, BrowserRunner $browserRunner): BrowserContainer
-    {
-        $browserContainer = new BrowserContainer(
-            $name,
-            $browserRunner->getImageName(),
-            $browserRunner->getLocalTargetPath(),
-            self::RUNNER_PATH
-        );
-
-        $setupResult = $browserContainer->setup();
-        self::assertProcessResult($setupResult);
-
-        return $browserContainer;
+        return SuiteManifest::fromArray($manifestData);
     }
 
     private static function assertRunnerOutput(Output $output): void
