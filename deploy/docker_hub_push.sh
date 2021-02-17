8c4eaa820b662ad1dbf7b14861d9f4ccd8a40a64
Jon Cram <webignition@gmail.com>
2021-02-17 14:41:59 +0000

    Remove obsolete dockerhub deploy scripts

--- deploy/docker_hub_push.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-source "$(pwd)"/build/.image_data.sh
-source "$(pwd)"/deploy/docker_hub_login.sh
-
-for IMAGE_NAME in "${IMAGE_NAMES[@]}"; do
-  docker push ${IMAGE_NAME}
-done
