a5265b0f3841d042d3c9c303851cb2de7acf294b
Jon Cram <webignition@gmail.com>
2021-02-23 10:31:15 +0000

    Tidy up build/build.sh

--- build/build.sh
+++ build/build.sh
@@ -1,14 +1,4 @@
-#!/usr/bin/env bash
+!/usr/bin/env bash
 
-source "$(pwd)"/build/.image_data.sh
-
-for IMAGE_NAME in "${IMAGE_NAMES[@]}"; do
-  TARGET=$(echo ${IMAGE_NAME} | cut -d ':' -f 1 | cut -d '/' -f 2)
-  echo "Building \"${IMAGE_NAME}\" with target \"${TARGET}\""
-
-  DOCKER_BUILDKIT=1 docker build \
-   --build-arg browser=${IMAGE_NAME} \
-   --build-arg BUILDKIT_INLINE_CACHE=1 \
-   --target=${TARGET} \
-   -t ${IMAGE_NAME} .
-done
+docker build -f chrome.Dockerfile -t "smartassert/chrome-runner:${TAG_NAME:-master}" .
+docker build -f firefox.Dockerfile -t "smartassert/firefox-runner:${TAG_NAME:-master}" .
